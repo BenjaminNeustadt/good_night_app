a21a5c589f80da0d63d8fb76c8b823ac45262f90
benjamin <b.james.neustadt@gmail.com>
2023-03-31 19:27:49 +0200

    Create "clocked_in"/"clocked_out" attributes
    * display them on the api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,13 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    render json: @users.as_json(include: { sleeps: {except: :user_id, methods: :updated_at}})
+    render json: @users.as_json(include: { sleeps: {except: :user_id, methods: [:clocked_out, :clocked_in], only: [:id, :clocked_in, :clocked_out]}})
     .map { |user| add_sleep_in_progress(user)}
   end
 
   def add_sleep_in_progress(user)
     user['sleeps'].each do |sleep|
-      sleep['updated_at'] = 'sleep in progress' if sleep['created_at'] == sleep['updated_at']
+      sleep['clocked_out'] = 'sleep in progress' if sleep['clocked_in'] == sleep['clocked_out']
     end
     user
   end
--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -1,3 +1,12 @@
 class Sleep < ApplicationRecord
   belongs_to :user, touch: true
+
+  def clocked_in
+    self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+
+  def clocked_out
+    self.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+  end
+
 end
