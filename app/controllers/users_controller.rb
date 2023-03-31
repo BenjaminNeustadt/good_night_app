c9b254b48c3b6b4134bcaf82d05c805de14cdca2
benjamin <b.james.neustadt@gmail.com>
2023-03-31 18:51:17 +0200

    Update index method return
    * Update should be present so that we have grounds on which to establish if the sleep is
    ongoing or complete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-    render json: @users, include: [:sleeps => {:except => [:user_id, :updated_at]}]
+    render json: @users, include: [:sleeps => {:except => :user_id}]
   end
 
   # GET /users/1
