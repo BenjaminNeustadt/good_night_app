03a7915e9473b21fd4f2c0687f2a5c14a987460a
benjamin <b.james.neustadt@gmail.com>
2023-03-31 18:31:28 +0200

    Update User controller to render user and sleeps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   def index
     @users = User.all
-
-    render json: @users
+    render json: @users, include: [:sleeps => {:except => [:user_id, :updated_at]}]
   end
 
   # GET /users/1
