cfc7a30a0be344a8be5b69d6846c7c5edcf04a81
benjamin <b.james.neustadt@gmail.com>
2023-03-31 18:27:19 +0200

    Generate user/sleep resource
    * Establish database column names in the migrations
    * Establish relationship between a user and sleeps in the models

---
+++ db/migrate/20230331161806_create_sleeps.rb
@@ -0,0 +1,8 @@
+class CreateSleeps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :sleeps do |t|
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
