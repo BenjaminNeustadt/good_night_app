d08ae565c910bb32d62027fcc71031b32889e741
benjamin <b.james.neustadt@gmail.com>
2023-03-31 19:43:39 +0200

    Define follow/unfollow POST routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :follows
+  post '/users/:user_id/follow/:follower_id', to: "users#follow"
+  post '/users/:user_id/unfollow/:follower_id', to: "users#unfollow"
   resources :users do
     resources :sleeps
     post 'clock_in', to: 'users#clock_in'
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
