133d5b896e33c3a92788cc90c52de444080935cb
benjamin <b.james.neustadt@gmail.com>
2023-04-21 15:16:07 +0200

    Modify: seed data should be more demonstrative

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,29 @@
+# Seed users
+user1 = User.create(name: "Jon")
+user2 = User.create(name: "Dan")
+user3 = User.create(name: "Tony")
+user4 = User.create(name: "Abdul")
+user5 = User.create(name: "Kwame")
 
-user_1 = User.create(name: "Jon")
-user_1.sleeps.create(created_at: Time.now - 20.hours, updated_at: Time.now - 6.hours)
+# Seed sleeps
+sleep1  = Sleep.create(created_at: Time.now - 7.hours, updated_at: Time.now, user: user1)
+sleep2  = Sleep.create(created_at: Time.now - 8.hours, updated_at: Time.now, user: user1)
+sleep3  = Sleep.create(created_at: Time.now - 6.hours, updated_at: Time.now, user: user1)
+sleep4  = Sleep.create(created_at: Time.now - 5.hours, updated_at: "Still sleeping...", user: user2)
+sleep5  = Sleep.create(created_at: Time.now - 9.hours, updated_at: Time.now, user: user2)
+sleep6  = Sleep.create(created_at: Time.now - 11.hours, updated_at: Time.now, user: user3)
+sleep7  = Sleep.create(created_at: Time.now - 9.hours, updated_at: "Still sleeping...", user: user4)
+sleep8  = Sleep.create(created_at: Time.now - 8.hours, updated_at: "Still sleeping...", user: user4)
+sleep9  = Sleep.create(created_at: Time.now - 7.hours, updated_at: Time.now, user: user5)
+sleep10 = Sleep.create(created_at: Time.now - 6.hours, updated_at: Time.now, user: user5)
+sleep11 = Sleep.create(created_at: Time.now - 6.hours, updated_at: Time.now, user: user5)
+sleep12 = Sleep.create(created_at: Time.now - 7.hours, updated_at: Time.now, user: user5)
+sleep13 = Sleep.create(created_at: Time.now - 7.hours, updated_at: "Still sleeping...", user: user5)
 
-user_2 = User.create(name: "Dan")
-user_2.sleeps.create(created_at: Time.now - 15.hours, updated_at: Time.now - 5.hours)
+# Seed followers
 
-user_3 = User.create(name: "Tony")
-user_3.sleeps.create(created_at: Time.now - 12.hours, updated_at: Time.now - 9.hours)
-user_3.followers.create(name: "Stacy")
-
-user_4 = User.create(name: "Franko")
-user_4.sleeps.create(created_at: Time.now - 10.hours, updated_at: Time.now - 9.hours)
+Follow.create(follower: user1, followee: user2)
+Follow.create(follower: user2, followee: user3)
+Follow.create(follower: user3, followee: user1)
+Follow.create(follower: user5, followee: user1)
+Follow.create(follower: user4, followee: user5)
