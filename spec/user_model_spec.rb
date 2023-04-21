f1c734fe0144f0e38fd0f820b031942308c63927
benjamin <b.james.neustadt@gmail.com>
2023-04-21 16:52:51 +0200

    Add test for checking the ordering of sleeps in records

--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -6,15 +6,16 @@ RSpec.describe "UsersModel" do
 
     it 'contains the sleep records of friends' do
       
+      # Create users
       user1 = User.create(name: 'User 1')
       user2 = User.create(name: 'User 2')
       user3 = User.create(name: 'User 3')
 
+      # Create sleeps for users
       sleep_record1 = Sleep.create(created_at: "2023-03-31T16:33:00.000Z", updated_at: "2023-03-31T18:33:00.000Z", user: user1)
       sleep_record2 = Sleep.create(created_at: "2023-03-31T16:33:00.000Z", updated_at: "2023-03-31T19:33:00.000Z", user: user1)
 
       # Create friend relationships
-
       Follow.create(follower: user1, followee: user2)
       Follow.create(follower: user1, followee: user3)
 
@@ -22,6 +23,26 @@ RSpec.describe "UsersModel" do
 
       expect(user2.friends_sleeps.first[:friend_name]).to eq "User 1"
       expect(user2.friends_sleeps.first).to include(expected_sleep_record.first)
+    end
+
+    it 'arranges sleeps according to length of sleep' do
+      
+      user1 = User.create(name: 'User 1')
+      user2 = User.create(name: 'User 2')
+      user3 = User.create(name: 'User 3')
+
+      # Create sleeps for users
+      sleep_record1 = Sleep.create(created_at: "2023-03-31T16:33:00.000Z", updated_at: "2023-03-31T18:33:00.000Z", user: user1)
+      sleep_record2 = Sleep.create(created_at: "2023-03-31T16:33:00.000Z", updated_at: "2023-03-31T19:33:00.000Z", user: user1)
+
+      # Create friend relationships
+      Follow.create(follower: user1, followee: user2)
+      Follow.create(follower: user1, followee: user3)
+
+      expected_sleep_record = [{:friend_name=>"User 1", :sleep_length=>120.0}, {:friend_name=>"User 1", :sleep_length=>180}]
+
+      expect(user2.friends_sleeps.first).to eq(expected_sleep_record[0])
+      expect(user2.friends_sleeps.last).to eq(expected_sleep_record[1])
     end
   end
 
