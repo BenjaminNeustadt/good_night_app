4c5df2b781d5340889295b17d12c98a46cc7b559
benjamin <b.james.neustadt@gmail.com>
2023-04-24 17:23:00 +0200

    Housekeeping: fix indentation

--- app/controllers/sleeps_controller.rb
+++ app/controllers/sleeps_controller.rb
@@ -3,13 +3,13 @@ class SleepsController < ApplicationController
 
   # POST /sleeps/track(params: [user_id])
   def track
-      @user.is_still_sleeping? ? @user.end_sleep : @user.start_sleep
-      render json: @user, include: :sleeps
+    @user.is_still_sleeping? ? @user.end_sleep : @user.start_sleep
+    render json: @user, include: :sleeps
   end
 
   private
 
   def set_user
-      @user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
   end
 end
