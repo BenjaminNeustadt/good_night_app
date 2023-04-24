97a4b7f54f84bf68075af0d05145935ad662eb56
benjamin <b.james.neustadt@gmail.com>
2023-04-24 17:05:00 +0200

    Remove obsolete migration files
    * Create new migration version

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_173025) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_150530) do
   create_table "follows", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followee_id"
+    t.integer "follower_id", null: false
+    t.integer "followee_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["followee_id"], name: "index_follows_on_followee_id"
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "sleeps", force: :cascade do |t|
@@ -31,5 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_173025) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "follows", "users", column: "followee_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "sleeps", "users"
 end
